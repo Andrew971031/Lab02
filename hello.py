e912d3195f5cbab8ef1dd4224c20e31f65e44de1
Andrew971031 <z5125854@unsw.edu.au>
2017-08-08 17:40:09 +1000

    massage

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 print("Hello, World!")
-<<<<<<< HEAD
-kjujgfg
+
 print("This is a git branch")
 "This is branch dev"
-=======
+
 print("This is a git branch")
 "This is branch master"
->>>>>>> master
+
+hello word!!
